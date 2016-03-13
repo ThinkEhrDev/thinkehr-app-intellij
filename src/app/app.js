169d63e5befce3b1ba93814ca2a8990083982697
Matija Kejzar <matija.kejzar@marand.si>
2016-03-13 17:42:31 +0100

    ng-route stub.

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,21 @@
-(function(angular) {
+(function (angular) {
 
-    var app = angular.module('ThinkEhrApp', [])
-        .controller('AppCtrl', [function() {
+    var app = angular.module('ThinkEhrApp', ['ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when("/", {
+                    templateUrl: 'app/views/tfpView.html'
 
-            }])
+                }).otherwise(
+                {
+                    redirectTo: '/'
+                }
+            );
+
+        }])
+        .controller('AppCtrl', [function () {
+
+        }])
         ;
 
 }(angular));
