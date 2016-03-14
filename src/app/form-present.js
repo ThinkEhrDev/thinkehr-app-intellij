10901e37b1c37ebea43bd0883b53dcd38eb5cdae
Matija Kejzar <matija.kejzar@marand.si>
2016-03-14 23:35:06 +0100

    tfpValue and tfpContainer

--- src/app/form-present.js
+++ src/app/form-present.js
@@ -1,19 +1,53 @@
 (function(angular) {
 
     angular.module('ThinkEhrFormPresent', [])
-        .directive('tfpPresent', [function () {
-
+        .directive('tfpValue', [function () {
             return {
-                restrict: 'A',
+                templateUrl: 'app/views/tfpValue.html',
+                restrict: 'E',
                 scope: {
-                    tfpPresent: "="
+                    ehrValue: '='
                 },
                 link: function (scope, element, attrs) {
-                    console.log("tfpPresent", scope.tfpPresent);
+
+                }
+            };
+        }])
+        .directive('tfpContainer', ['$compile', '$rootScope', function ($compile, $rootScope) {
+
+            return {
+                templateUrl: 'app/views/tfpContainer.html',
+                restrict: 'E',
+                scope: {
+                    ehrContainer: '='
+                },
+                link: function (scope, element, attrs) {
+
+                    angular.forEach(scope.ehrContainer.children, function (child) {
+                        var childScope = $rootScope.$new(false, scope);
+                        childScope.tfpElement = child;
+
+                        if (isContainer(child)) {
+                            var html = '<tfp-container ehr-container="tfpElement" data-rm-type="{{ehrContainer.rmType}}"></tfp-container>'
+                        } else {
+                            html = '<tfp-value ehr-value="tfpElement" data-rm-type="{{ehrValue.rmType}}"></tfp-value>'
+                        }
+
+                        var childElement = $compile(html)(childScope);
+                        element.append(childElement);
+                    });
+
+                    function isContainer(child) {
+                        return child.hasOwnProperty('children');
+                    }
+
                 }
             };
 
         }])
+
+
+
         ;
 
 }(angular));
